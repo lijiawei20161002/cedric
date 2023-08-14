2542c3f386348ec6df775fffa602cc7fcecc7a53
李佳蔚 <lijiawei@MININT-9BDOM63.redmond.corp.microsoft.com>
2023-08-14 09:55:31 +0800

    run large scale simulation

--- small_simulation/output/plot/plot_reward.py
+++ small_simulation/output/plot/plot_reward.py
@@ -15,5 +15,7 @@ agent = 5
 df = pd.DataFrame({'no credit': df1[str(agent)], 'cedric': df2[str(agent)], 'counterfactual': df3[str(agent)], "shared": df4[str(agent)]})
 for i in range(1, len(df)):
     df.iloc[i] = df.iloc[i] + df.iloc[i-1]
-df.plot(xlabel='training episodes', ylabel='cumulative reward')
+df.plot()
+plt.xlabel('training episodes', fontsize=16)
+plt.ylabel('cumulative reward', fontsize=16)
 plt.savefig('reward.png')
